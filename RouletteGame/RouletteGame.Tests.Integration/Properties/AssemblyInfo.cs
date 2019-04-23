c5c2af4d25162007d2895a7297625623ec14116a
florent417 <flole@live.dk>
2019-04-23 13:23:14 +0200

    Added the real roulette solution game

--- RouletteGame/RouletteGame.Tests.Integration/Properties/AssemblyInfo.cs
+++ RouletteGame/RouletteGame.Tests.Integration/Properties/AssemblyInfo.cs
@@ -4,25 +4,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("RouletteGame.Legacy")]
+[assembly: AssemblyTitle("Game.Tests.Integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RouletteGame.Legacy")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyProduct("Game.Tests.Integration")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("aed132a0-fdc9-4731-a6e2-ded63cb20988")]
+[assembly: Guid("e853348c-f49d-440a-bf28-5c5af113c3e1")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
