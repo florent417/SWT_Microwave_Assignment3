233539443b29c7db74ae9c18e62064404e192672
Oliver <oliver_mei@hotmail.com>
2019-04-24 11:17:19 +0200

    projekttype ændret for integration test projekt

---
+++ ClassLibrary1/1st_Integration_cookcontroler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Class1
+    {
+        private ITimer tim;
+        private IDisplay dis;
+        private IPowerTube pow;
+        private ICookController uut;
+        private IOutput output;
+        private IUserInterface ui;
+
+        [SetUp]
+        public void SetUp()
+        {
+            output = Substitute.For<IOutput>();
+            tim = new Timer();
+            dis = new Display(output);
+            pow = new PowerTube(output);
+            ui = Substitute.For<IUserInterface>();
+            uut = new CookController(tim, dis, pow, ui);
+        }
+
+        [Test]
+        public void startcook_powertubeoutput()
+        {
+            uut.StartCooking(95, 5);
+            output.Received().OutputLine(Arg.Is<string>(str => str.ToLower().Contains("PowerTube works with 95 %")));
+        }
+    }
+}
