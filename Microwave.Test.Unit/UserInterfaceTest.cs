e3e4cb3265df3c234f3cc462fc77699f888630fc
florent417 <flole@live.dk>
2019-04-25 14:07:11 +0200

    Forgot something

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -156,7 +156,7 @@ namespace Microwave.Test.Unit
             // Now in SetTime
             startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
 
-            cooker.Received(1).StartCooking(50, 60);
+            cooker.Received(1).StartCooking(50/7, 60);
         }
 
         [Test]
@@ -197,7 +197,7 @@ namespace Microwave.Test.Unit
             // Should call with correct values
             startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
 
-            cooker.Received(1).StartCooking(100, 120);
+            cooker.Received(1).StartCooking(100/7, 120);
         }
 
         [Test]
@@ -214,7 +214,7 @@ namespace Microwave.Test.Unit
             // Should call with correct values
             startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
 
-            cooker.Received(1).StartCooking(700, 60);
+            cooker.Received(1).StartCooking(700/7, 60);
 
         }
 
